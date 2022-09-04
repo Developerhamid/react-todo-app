1dde573790f254f318c616eeca1457638eb7aa01
Developerhamid <iamdeveloperhamid@gmail.com>
2022-09-04 09:59:02 +0530

    Updates

---
+++ precache-manifest.7cf441fdb7a8a6e17d92319cd77b6f46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24ba11f01972ef542791",
+    "url": "/react-todo-app/static/css/main.b2cacf1e.chunk.css"
+  },
+  {
+    "revision": "24ba11f01972ef542791",
+    "url": "/react-todo-app/static/js/main.24ba11f0.chunk.js"
+  },
+  {
+    "revision": "b58f3ca4bbb2b84036cb",
+    "url": "/react-todo-app/static/js/1.b58f3ca4.chunk.js"
+  },
+  {
+    "revision": "dc884f14d85a35ea3dce",
+    "url": "/react-todo-app/static/js/runtime~main.dc884f14.js"
+  },
+  {
+    "revision": "19c5ebc704e01b157c181151389b0a6c",
+    "url": "/react-todo-app/index.html"
+  }
+];
